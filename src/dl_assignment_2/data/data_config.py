9d16ecb7c516a40c60f9d6d6eb1f5e847c28fb50
Alexderoode011202 <119018796+Alexderoode011202@users.noreply.github.com>
2026-05-11 23:22:38 +0200

    added CROSS_TRAIN config var

--- src/dl_assignment_2/data/data_config.py
+++ src/dl_assignment_2/data/data_config.py
@@ -3,6 +3,7 @@ from typing import List
 
 CROSS_ROOT: str = "datasets/Cross"
 CROSS_TEST_FOLDERS: List[str] = ["test1", "test2", "test3"]
+CROSS_TRAIN: str = "datasets/Cross/train"
 
 INTRA_ROOT: str = "datasets/Intra"
 INTRA_TRAIN: str = 'datasets/Intra/train'
