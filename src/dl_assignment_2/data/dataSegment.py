c4ac8cff38c33c2848cce5f5a76fcd95965819bb
Alexderoode011202 <119018796+Alexderoode011202@users.noreply.github.com>
2026-05-31 18:56:59 +0200

    minor refactoring

--- src/dl_assignment_2/data/dataSegment.py
+++ src/dl_assignment_2/data/dataSegment.py
@@ -4,6 +4,7 @@ import h5py
 from h5py import File
 from numpy import ndarray, array, delete, zeros, concatenate
 import matplotlib.pyplot as plt
+from matplotlib import axes
 
 from typing import Tuple, Callable, Optional
 from dataclasses import dataclass
@@ -39,26 +40,6 @@ class DataSegment:
             raise ValueError("relative path AND dataSegmentInfo variable have been left unspecified.")
         
         return None
-
-    def _construct_segment_from_DataSegmentInfo(self, dataSegmentInfo: "DataSegmentInfo") -> None:
-        self.data: ndarray = dataSegmentInfo.data
-        self.subject_id: int = dataSegmentInfo.subject_id
-        self.task: str = dataSegmentInfo.task
-        self.segment: int = dataSegmentInfo.segment
-
-        return None
-        
-    def _construct_segment_from_file(self, relative_path):
-        task, subject_id, segment = self._get_metadata(relative_path)
-        self.task: str = task
-        self.subject_id: int = subject_id
-        self.segment: int = segment
-
-        self.data: ndarray = self._read_segment_from_file(relative_path)
-        #//<<
-
-    def __repr__(self) -> str:
-        return f"data segment concerning subject: {self.subject_id} for task: {self.task} at segment: {self.segment}."
     
     def transform(self, transformation_function: Callable) -> "DataSegment":
         data = transformation_function(self.data)
@@ -67,7 +48,91 @@ class DataSegment:
                                subject_id=self.subject_id, 
                                task=self.task, 
                                segment=self.segment))
+
+    # all the analytical functions:
+    ###############################
+    def plot(self) -> None:
+        plt.imshow(self.data, aspect='auto')
+        plt.show()
+        return None
     
+    def trim_n_rows(self, n: int = 3) -> "DataSegment":
+        "removes all rows but every N one"
+        
+        kept_indices: list[int] = self._compute_indices_to_keep(n)
+        removed_indices: list[int] = self._compute_indices_to_remove(kept_indices)
+
+        trimmed_data: ndarray
+        if len(self.data.shape) == 2:
+            trimmed_data =  delete(self.data, removed_indices, 1)
+
+        else:
+            trimmed_data = delete(self.data, removed_indices) 
+            
+        return DataSegment(dataSegmentInfo=DataSegmentInfo(trimmed_data, self.subject_id, self.task, self.segment))
+
+        
+    def get_residuals(self) -> "DataSegment":
+        """Computes matrix where each column i consists of the difference
+        between the the columns i and i+1 of the DataSegment's data
+        """
+        # 1. construct the padding column:
+        row_amount: int = self.data.shape[0]
+        padding_column: ndarray = zeros(shape=(row_amount, 1))
+        
+        # 2. construct the right-shifted copy of the data:
+        copy_without_final_column: ndarray = self.data.copy()[:,:-1]
+        padded_right_shifted_copy: ndarray = concatenate((padding_column, copy_without_final_column), axis=1)
+        
+        # 3. construct the residuals:
+        padded_residuals: ndarray = self.data - padded_right_shifted_copy
+        
+        # 4. remove the padded first column
+        unpadded_residuals: ndarray = padded_residuals[:,1:]
+        
+        return DataSegment(dataSegmentInfo=DataSegmentInfo(unpadded_residuals, self.subject_id, self.task, self.segment))
+
+
+    # all the getters:
+    ##################
+    def get_data(self) -> ndarray:
+        #//>>
+        return self.data
+        #//<<
+    
+    def get_subject_id(self) -> int:
+        #//>>
+        return self.subject_id
+        #//<<
+    
+    def get_task(self) -> str:
+        #//>>
+        return self.task
+        #//<<
+    
+    def get_segment(self) -> int:
+        #//>>
+        return self.segment
+        #//<<
+    
+    # init helpers:
+    ###############
+    def _construct_segment_from_DataSegmentInfo(self, dataSegmentInfo: "DataSegmentInfo") -> None:
+        self.data: ndarray = dataSegmentInfo.data
+        self.subject_id: int = dataSegmentInfo.subject_id
+        self.task: str = dataSegmentInfo.task
+        self.segment: int = dataSegmentInfo.segment
+        return None
+        
+    def _construct_segment_from_file(self, relative_path):
+        task, subject_id, segment = self._get_metadata_from_filename(relative_path)
+        self.task: str = task
+        self.subject_id: int = subject_id
+        self.segment: int = segment
+
+        self.data: ndarray = self._read_segment_from_file(relative_path)
+        #//<<
+        
     def _read_segment_from_file(self, path: str) -> ndarray:
         #//>>
         file: File = h5py.File(path, 'r')
@@ -77,9 +142,12 @@ class DataSegment:
 
         return data
         #//<<
-
+        
+    def __repr__(self) -> str:
+        return f"data segment concerning subject: {self.subject_id} for task: {self.task} at segment: {self.segment}."
+    
     @staticmethod
-    def _get_metadata(path: str) -> Tuple[str, int, int]:
+    def _get_metadata_from_filename(path: str) -> Tuple[str, int, int]:
         #//>>
         file_name_without_extension: str = DataSegment._get_filename_without_extension(path)
 
@@ -114,46 +182,6 @@ class DataSegment:
         return file_name_without_extension
         #//<<
 
-    # all the getters:
-    def get_data(self) -> ndarray:
-        #//>>
-        return self.data
-        #//<<
-    
-    def get_subject_id(self) -> int:
-        #//>>
-        return self.subject_id
-        #//<<
-    
-    def get_task(self) -> str:
-        #//>>
-        return self.task
-        #//<<
-    
-    def get_segment(self) -> int:
-        #//>>
-        return self.segment
-        #//<<
-
-    def plot(self) -> None:
-        plt.imshow(self.data, aspect='auto')
-        plt.show()
-        return None
-    
-    def trim_n_rows(self, n: int = 3) -> "DataSegment":
-        "removes all rows but every N one"
-        
-        kept_indices: list[int] = self._compute_indices_to_keep(n)
-        removed_indices: list[int] = self._compute_indices_to_remove(kept_indices)
-
-        trimmed_data: ndarray
-        if len(self.data.shape) == 2:
-            trimmed_data =  delete(self.data, removed_indices, 1)
-
-        else:
-            trimmed_data = delete(self.data, removed_indices) 
-            
-        return DataSegment(dataSegmentInfo=DataSegmentInfo(trimmed_data, self.subject_id, self.task, self.segment))
     
         
     def _compute_indices_to_keep(self, step) -> list[int]:
@@ -172,28 +200,6 @@ class DataSegment:
         return list(set(range(self.data.shape[1])).difference(indices_to_keep))
 
 
-    def get_residuals(self) -> "DataSegment":
-        """Computes matrix where each column i consists of the difference
-        between the the columns i and i+1 of the DataSegment's data
-        """
-        
-        
-        
-        # 1. construct the right-shifted copy of the data:
-        copy_without_final_column: ndarray = self.data.copy()[:,:-1]
-        padded_right_shifted_copy: ndarray = concatenate((padding_column, copy_without_final_column), axis=1)
-        
-        # 2. construct the padding column:
-        row_amount: int = self.data.shape[0]
-        padding_column: ndarray = zeros(shape=(row_amount, 1))
-        
-        # 3. construct the residuals:
-        padded_residuals: ndarray = self.data - padded_right_shifted_copy
-        
-        # 4. remove the padded first column
-        unpadded_residuals: ndarray = padded_residuals[:,1:]
-        
-        return DataSegment(dataSegmentInfo=DataSegmentInfo(unpadded_residuals, self.subject_id, self.task, self.segment))
 
 
 
