e9cc718463b3c1363f1491b772f048cbb8023328
Alexderoode011202 <119018796+Alexderoode011202@users.noreply.github.com>
2026-06-03 12:47:29 +0200

    added abstract stuff in

--- src/dl_assignment_2/modeling/model.py
+++ src/dl_assignment_2/modeling/model.py
@@ -2,10 +2,10 @@ from torch import nn
 from abc import ABC, abstractmethod
 
 class BaseModel(nn.Module, ABC):
+    @abstractmethod
     def __init__(self, input_dim: int, hidden_dim: int):
         pass
-
+    
+    @abstractmethod
     def forward(self, x):
         pass
-
-    
