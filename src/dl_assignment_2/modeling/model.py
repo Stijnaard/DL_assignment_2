7a7feee6db2847f6ef077d284300f0f56d4c6374
Alexderoode011202 <119018796+Alexderoode011202@users.noreply.github.com>
2026-06-04 00:11:48 +0200

    added some tests for model but are not finished yet

--- src/dl_assignment_2/modeling/model.py
+++ src/dl_assignment_2/modeling/model.py
@@ -1,9 +1,12 @@
-from torch import nn
 from abc import ABC, abstractmethod
 
-from torch.nn import RNN, Linear
+from torch.nn import RNN, Linear, Softmax
+from torch import nn, Tensor
 
 class BaseModel(nn.Module, ABC):
+    singular_sigmoid: Softmax = Softmax(dim=1)
+    batch_sigmoid: Softmax = Softmax(dim=2)
+    
     @abstractmethod
     def __init__(self):
         super().__init__()
@@ -12,6 +15,20 @@ class BaseModel(nn.Module, ABC):
     def forward(self, x):
         pass
     
+    def predict(self, X: Tensor) -> Tensor:
+        logits: Tensor = self(X)
+        y = self.get_sigmoid(X, logits)
+        return y
+
+    def get_sigmoid(self, X, logits):
+        if len(X.shape) == 2:
+            y: Tensor = self.singular_sigmoid(logits)
+        elif len(X.shape) == 3:
+            y: Tensor = self.batch_sigmoid(logits)
+        else:
+            raise ValueError("X's dimensionality exceeds model")
+        return y
+        
 class SimpleRNN(BaseModel):
     def __init__(self, in_dim, hidden_dim: int, out_dim: int):
         super().__init__()
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,16 +1,37 @@
+from torch import all
+
 from dl_assignment_2.modeling.model import SimpleRNN
 
 from torch import Tensor, ones
-class Test_model:
+
+class Test_output_shapes:
     rnn = SimpleRNN(10,20,4)
-    def test_correct_output_shape(self):
+    def test_one(self):
         x: Tensor = ones((3, 10))   # BATCH, SEQ LEN, INPUT DIM
-        
         y = self.rnn(x)
         assert y.shape == (1,4)
         
-    def test_batch_correct_output_shape(self):
+    def test_batch(self):
         x: Tensor = ones(5,3, 10)
         y = self.rnn(x)
-        
         assert y.shape == (1, 5, 4) # always adds extra dimension at axis=0
+        
+class Test_Predicion_Sanity:
+    rnn = SimpleRNN(10,20,4)
+    
+    def test_one(self):
+        x: Tensor = ones((3, 10))   # BATCH, SEQ LEN, INPUT DIM
+        y = self.rnn.predict(x)
+        logits = self.rnn(x)
+        assert y.sum(1) == Tensor([1])
+        assert not all(logits == y)
+        
+    def test_batch(self):
+        x: Tensor = ones((2, 3, 10))   # BATCH, SEQ LEN, INPUT DIM
+        y = self.rnn.predict(x)
+        logits = self.rnn(x)
+        print(x.shape)
+        print(logits)
+        print(y)
+        assert y.sum(1).item() == 1
+        assert y.shape == (2, 4)    
