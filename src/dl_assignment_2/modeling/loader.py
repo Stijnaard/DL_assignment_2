3c7948290b254bf605c6181b408b72cc80f51bf0
Alexderoode011202 <119018796+Alexderoode011202@users.noreply.github.com>
2026-06-03 12:09:24 +0200

    wrote custom dataset class

--- src/dl_assignment_2/modeling/loader.py
+++ src/dl_assignment_2/modeling/loader.py
@@ -1,32 +1,37 @@
 from dl_assignment_2.data.dataSegment import DataSegment
 from dl_assignment_2.data.pipeline import Pipeline
+from dl_assignment_2.data.config import TASK_TYPES
 
 from typing import Sequence, Optional, Callable
 
 from torch.utils.data import Dataset
-from torch import Tensor
-from numpy import ndarray
+from torch import Tensor, tensor
+from numpy import ndarray, expand_dims, concatenate
 
 class CustomDataset(Dataset):
-    feature_matrix: Tensor
+    feature_matrix: Tensor # (N, sensors, T)
     label_vector: Tensor
+    device: str
 
     def __init__(self, segments: Sequence[DataSegment], pipeline: Optional[Pipeline] = None) -> None:
+        
+
         # 1. perform the transformations if you provide a pipeline:
         if pipeline:
             for segment in segments:
                 segment = pipeline(segment)
 
         # 2. get the data from the segments:
-        matrices: list[ndarray] = [segment.get_data() for segment in segments]
-    
-        
+        matrices: list[ndarray] = [expand_dims(segment.get_data(), 0) for segment in segments]
 
-        
+        self.feature_matrix: Tensor = tensor(concatenate(matrices, axis=0))
 
-
+        sorted_tasks = sorted(TASK_TYPES)
+        self.label_vector: Tensor = tensor([sorted_tasks.index(segment.get_task()) for segment in segments])
+        print(sorted_tasks)
     def __len__(self) -> int:
-        pass
+        return len(self.label_vector)
 
     def __getitem__(self, index):
-        pass
+        return self.feature_matrix[index,:,:], self.label_vector[index]
+    
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,23 +1,55 @@
 from dl_assignment_2.modeling.loader import CustomDataset
 from dl_assignment_2.data.dataSegment import DataSegment, SegmentInfo
 
-from numpy import ndarray, array, all
+from numpy import ndarray, array
 from torch.utils.data import DataLoader
+from torch import tensor, all
 
-class Test_Dataset:
+class Test_Dataset_single_instances:
     m1: ndarray = array([[1,2,3,4], [5,6,7,8]])
     m2: ndarray = m1*-1
 
     s1: DataSegment = DataSegment(info=SegmentInfo(m1, 123456, "rest", 1))
-    s2: DataSegment = DataSegment(info=SegmentInfo(m1, 123456, "task_motor", 1))
+    s2: DataSegment = DataSegment(info=SegmentInfo(m2, 123456, "task_motor", 1))
+
+    ds: CustomDataset = CustomDataset((s1, s2))
+
+
 
     def test_correct_feature_matrix(self):
-        ds: CustomDataset = CustomDataset((self.s1, self.s2))
-        x, y = ds.__getitem__(0)
-        assert all(x == array([[1,2,3,4], [5,6,7,8]]))
+        x, _ = self.ds.__getitem__(0)
+        assert all(x == tensor([[1,2,3,4], [5,6,7,8]]))
         
 
     def test_correct_label(self):
-        ds: CustomDataset = CustomDataset((self.s1, self.s2))
-        x, y = ds.__getitem__(0)
-        assert y == 3
+        _, y = self.ds.__getitem__(0)
+        assert y == 0   # 0 = rest (moves alphabetically)
+
+
+class Test_Dataset_Batches:
+    m1: ndarray = array([[1,2,3,4], [5,6,7,8]])
+    m2: ndarray = m1*-1
+
+    s1: DataSegment = DataSegment(info=SegmentInfo(m1, 123456, "rest", 1))
+    s2: DataSegment = DataSegment(info=SegmentInfo(m2, 123456, "task_motor", 1))
+
+    ds: CustomDataset = CustomDataset((s1, s2))
+    dl: DataLoader = DataLoader(ds, 2)
+
+    def test_correct_feature_batch(self):
+        for X, _ in self.dl:
+            assert all(X == tensor([[[1,2,3,4], 
+                                     [5,6,7,8]],[
+                                     [-1,-2,-3,-4], [-5,-6,-7,-8]]]))
+            
+    def test_correct_label_batch(self):
+        for _, y in self.dl:
+            assert all(y == tensor([0,1]))
+            
+    def test_correct_batch_shape(self):
+        for X, _ in self.dl:
+            assert X.shape == (2,2,4)
+
+    def 
+
+    
