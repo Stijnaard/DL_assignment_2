5e3a608d023ad8aed2db1a61f8795b8eeb3064ba
Alexderoode011202 <119018796+Alexderoode011202@users.noreply.github.com>
2026-06-03 09:49:39 +0200

    wrote skeleton for pipeline and loader

---
+++ src/dl_assignment_2/data/pipeline.py
@@ -0,0 +1,10 @@
+from dl_assignment_2.data.dataSegment import DataSegment
+
+class Pipeline:
+    def __init__(self, transformations) -> None:
+        pass
+
+    def __call__(segment: DataSegment) -> DataSegment:
+        pass
+
+    
--- src/dl_assignment_2/modeling/loader.py
+++ src/dl_assignment_2/modeling/loader.py
@@ -1,11 +1,12 @@
 from dl_assignment_2.data.dataSegment import DataSegment
-from typing import Sequence
+
+from typing import Sequence, Optional, Callable
 
 from torch.utils.data import Dataset
 
 
 class CustomDataset(Dataset):
-    def __init__(self, segments: Sequence[DataSegment]) -> None:
+    def __init__(self, segments: Sequence[DataSegment], transformations: Optional[Sequence[Callable]] = None) -> None:
         pass
 
     def __len__(self) -> int:
