f726fbd823d6fc0a4764e4a9f2d6d12392049fe4
Casjur <94750498+Casjur@users.noreply.github.com>
2026-06-15 17:55:34 +0200

    Fix InceptionTime input shape

--- src/dl_assignment_2/Casper_models/InceptionTime.py
+++ src/dl_assignment_2/Casper_models/InceptionTime.py
@@ -1,7 +1,16 @@
 from tsai.models.InceptionTime import InceptionTime as _InceptionTime
 
-InceptionTime = _InceptionTime
+#InceptionTime = _InceptionTime
+
+class InceptionTime(_InceptionTime):
+    def __init__(self, c_in, c_out, seq_len=None, nf=32, nb_filters=None, **kwargs):
+        super().__init__(c_in, c_out, seq_len, nf, nb_filters, **kwargs)
+
+    def forward(self, x):
+        """x: (batch, timepoints, channels) -> logits: (batch, c_out)"""
+        x = x.permute(0, 2, 1) # (N, T, sensors) -> (N, sensors, T)
+        return super().forward(x)
 
 __all__ = ["InceptionTime"]
 
-
+#test = InceptionTime(c_in=248, c_out=4, seq_len=512)
